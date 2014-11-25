16376bc38471eec7476a6ad2d53b8f40f413575d
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-11-25 12:13:08 +0100

    starting another small best-n script

---
+++ bestN_foreach_pos.pl
@@ -0,0 +1,7 @@
+#
+# a small script that will consume output_all_n_comb result, 
+# and output only best N patterns for each "start" (where each 
+# start means, say, if we have n1-n2-n3-n4, we will get best scoring 
+# n1-x-x-x for each n1. ) 
+
+# TODO 
